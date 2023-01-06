151e0b18a70be880e5437bd5d78ae2481ed2efef
sujean323 <90737530+sujean323@users.noreply.github.com>
2023-01-06 22:14:18 +0800

    Update basic2.c

--- ITSA/basic2.c
+++ ITSA/basic2.c
@@ -1,10 +1,11 @@
+//英哩轉公里
 #include<stdio.h>
 
 int main(){
     int input;
     double ans;
     while(scanf("%d", &input) != EOF){
-        ans = input*1.6;
+        ans = input*1.6; //1 英哩= 1.6 公里
         printf("%.1f\n", ans);
     }
 
